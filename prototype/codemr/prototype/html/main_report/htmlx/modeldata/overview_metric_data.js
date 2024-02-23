790f22b13affb627797c46d1ce56f9058180db86
Joao Felipe C Bourscheid <joao.felipe.c.b@gmail.com>
2024-02-22 23:16:13 -0300

    doc: code report analysis

---
+++ prototype/codemr/prototype/html/main_report/htmlx/modeldata/overview_metric_data.js
@@ -0,0 +1,1 @@
+var CodeMR_ProjectMetrics = {"Project Name":"prototype", "Number of Highly Problematic Classes":0, "Number of Entities":21, "Number of Problematic Classes":0, "Class Lines of Code":225, "Number of External Packages":12, "Number of Packages":2, "Number of External Entities":27}
---
+++ prototype/codemr/prototype/html/main_report/htmlx/modeldata/packagedependency.js
@@ -0,0 +1,8 @@
+var matrix = [[0,0],[2,0]]
+var packages = [{
+"name": " org.acme.prototype.model", "color": " #3182bd"
+}
+,{
+"name": " org.acme.prototype", "color": " #6baed6"
+}
+];
