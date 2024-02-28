8199f411b6e5132ad5e680e257511ff44d64bd7f
Joao Felipe C Bourscheid <joao.felipe.c.b@gmail.com>
2024-02-28 13:58:45 -0300

    chore: update code report and java doc

--- prototype/codemr/prototype/html/main_report/htmlx/modeldata/overview_metric_data.js
+++ prototype/codemr/prototype/html/main_report/htmlx/modeldata/overview_metric_data.js
@@ -1,1 +1,1 @@
-var CodeMR_ProjectMetrics = {"Project Name":"prototype", "Number of Highly Problematic Classes":0, "Number of Entities":21, "Number of Problematic Classes":0, "Class Lines of Code":225, "Number of External Packages":12, "Number of Packages":2, "Number of External Entities":27}
+var CodeMR_ProjectMetrics = {"Project Name":"prototype", "Number of Highly Problematic Classes":0, "Number of Entities":23, "Number of Problematic Classes":0, "Class Lines of Code":232, "Number of External Packages":13, "Number of Packages":3, "Number of External Entities":28}
